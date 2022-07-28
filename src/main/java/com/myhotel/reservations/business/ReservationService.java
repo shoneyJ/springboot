91f2738f444a9595971f1de721a0361cf92d719b
Shoney John <shoney.john@outlook.com>
2022-07-28 23:32:49 +0200

    feat: add service

--- src/main/java/com/myhotel/reservations/business/ReservationService.java
+++ src/main/java/com/myhotel/reservations/business/ReservationService.java
@@ -1,5 +1,11 @@
 package com.myhotel.reservations.business;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class ReservationService {
 
+    public String add(RoomReservation reservation){
+        return "wip";
+    }
 }
--- src/main/java/com/myhotel/reservations/controller/ReservationController.java
+++ src/main/java/com/myhotel/reservations/controller/ReservationController.java
@@ -1,5 +1,6 @@
 package com.myhotel.reservations.controller;
 
+import com.myhotel.reservations.business.ReservationService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,8 +9,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import com.myhotel.reservations.business.RoomReservation;
 
+import java.time.Clock;
+
 @Controller
 public class ReservationController {
+
+    private  final ReservationService reservationService;
+
+    public ReservationController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
 
     @GetMapping("/reservation")
     public String reservationForm(Model model) {
@@ -20,7 +29,9 @@ public class ReservationController {
     @PostMapping("/reservation")
     public String reservationSubmit(@ModelAttribute RoomReservation reservation, Model model) {
         model.addAttribute("reservation", reservation);
-        return "reservation";
+         System.out.println(this.reservationService.add(reservation));
+
+        return "reservationlist";
     }
 
 }
